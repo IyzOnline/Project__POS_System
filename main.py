c6af4749683e2852164ed26a722aa1d712abe8fe
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-12-11 02:29:09 +0800

    fix: reqForOrderHistory should have no arguments passed

--- main.py
+++ main.py
@@ -85,40 +85,13 @@ class App(tk.Tk) :
   def initHistoryPage(self) :
     historyPageFrame = tk.Frame(self.mainFrame, padx=10, pady=10, background="#ffffff")
     historyPageFrame.pack()
+
+    historyTableLbl = ttk.Label(historyPageFrame, text="Order History")
+    historyTableLbl.pack()
+
     self.displayHistoryTableColumns(historyPageFrame)
     self.displayHistoryTable(historyPageFrame)
 
-  def displayHistoryTable(self, parent):
-    historyTableLbl = ttk.Label(parent, text="Order History")
-    historyTableLbl.pack()
-
-    self.reqForOrderHistory(parent)
-    if not self.__OrderRecords:
-      print("No orders recorded")
-      return
-
-    for key, orderData in self.__OrderRecords.items():
-      for orderItemKey, orderItemData in orderData["items"].items():
-        orderItemRow = tk.Frame(parent)
-
-        orderIDCol = ttk.Label(orderItemRow, text=orderData["orderID"], style="Cell.TLabel", anchor="center")
-        dateCol = ttk.Label(orderItemRow, text=orderData["date"], style="Cell.TLabel", anchor="center")
-        nameCol = ttk.Label(orderItemRow, text=orderItemData["name"], style="Cell.TLabel", anchor="center")
-        categoryCol = ttk.Label(orderItemRow, text=orderItemData["category"], style="Cell.TLabel", anchor="center")
-        priceCol = ttk.Label(orderItemRow, text=f"₱{orderItemData['price']:.2f}", style="Cell.TLabel", anchor="center")
-        quantityCol = ttk.Label(orderItemRow, text=orderItemData["quantity"], style="Cell.TLabel", anchor="center")
-        totalCol = ttk.Label(orderItemRow, text=f"₱{orderItemData['quantity'] * orderItemData['price']:.2f}", style="Cell.TLabel", anchor="center")
-
-        orderIDCol.grid(column=0, row=0, sticky="nsew")
-        dateCol.grid(column=1, row=0, sticky="nsew")
-        nameCol.grid(column=2, row=0, sticky="nsew")
-        categoryCol.grid(column=3, row=0, sticky="nsew")
-        priceCol.grid(column=4, row=0, sticky="nsew")
-        quantityCol.grid(column=5, row=0, sticky="nsew")
-        totalCol.grid(column=6, row=0, sticky="nsew")
-        
-        orderItemRow.pack(fill="x")
-  
   def displayHistoryTableColumns(self, parent):
     columns = tk.Frame(parent)
 
@@ -149,13 +122,33 @@ class App(tk.Tk) :
     
     columns.pack(fill="x")
 
-  """
-    self.__OrderRecords[order[0]] = {
-        "orderID": order[0], 
-        "date": datetime.fromisoformat(order[1]),
-        "items": {}
-      }
-  """
+  def displayHistoryTable(self, parent):
+    self.reqForOrderHistory()
+    if not self.__OrderRecords:
+      print("No orders recorded")
+      return
+
+    for key, orderData in self.__OrderRecords.items():
+      for orderItemKey, orderItemData in orderData["items"].items():
+        orderItemRow = tk.Frame(parent)
+
+        orderIDCol = ttk.Label(orderItemRow, text=orderData["orderID"], style="Cell.TLabel", anchor="center")
+        dateCol = ttk.Label(orderItemRow, text=orderData["date"], style="Cell.TLabel", anchor="center")
+        nameCol = ttk.Label(orderItemRow, text=orderItemData["name"], style="Cell.TLabel", anchor="center")
+        categoryCol = ttk.Label(orderItemRow, text=orderItemData["category"], style="Cell.TLabel", anchor="center")
+        priceCol = ttk.Label(orderItemRow, text=f"₱{orderItemData['price']:.2f}", style="Cell.TLabel", anchor="center")
+        quantityCol = ttk.Label(orderItemRow, text=orderItemData["quantity"], style="Cell.TLabel", anchor="center")
+        totalCol = ttk.Label(orderItemRow, text=f"₱{orderItemData['quantity'] * orderItemData['price']:.2f}", style="Cell.TLabel", anchor="center")
+
+        orderIDCol.grid(column=0, row=0, sticky="nsew")
+        dateCol.grid(column=1, row=0, sticky="nsew")
+        nameCol.grid(column=2, row=0, sticky="nsew")
+        categoryCol.grid(column=3, row=0, sticky="nsew")
+        priceCol.grid(column=4, row=0, sticky="nsew")
+        quantityCol.grid(column=5, row=0, sticky="nsew")
+        totalCol.grid(column=6, row=0, sticky="nsew")
+        
+        orderItemRow.pack(fill="x")
 
   #Sidebar
   def initializeSidebar(self) :
